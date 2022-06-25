0d3dce74b63b811eebe1556ca66e3ea139fad5fa
koechronix <koechronix@yahoo.com>
2022-06-24 23:44:42 -0400

    initialized console

--- script.js
+++ script.js
@@ -20,32 +20,32 @@ function conversion(val) {
   }
 
   submitBtn.addEventListener("click", () => {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + text.value + "&appid=" + apiKey)
-      .then((response) => {
-        if (!response.ok) {
-          contentBox.style.display = "none";
-          alert("Check Your Spelling.");
-          throw new Error("Check Your Spelling.");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        // // console.log(data);
-        // let nameValue = data["name"];
-        // let humidityValue = data["main"]["humidity"];
-        // let tempValue = data["main"]["temp"];
-        // let windValue = data["wind"]["speed"];
-        // let descValue = data["weather"][0]["description"];
-        // let iconValue = data["weather"][0]["icon"];
-  
-        // city.innerHTML = `${nameValue}`;
-        // icon.src = "https://openweathermap.org/img/wn/" + iconValue + ".png";
-        // description.innerHTML = `Description: ${descValue}`;
-        // temp.innerHTML = `Temperature: ${conversion(tempValue)} °C`;
-        // humidity.innerHTML = `Humidity: ${humidityValue} %`;
-        // wind.innerHTML = `Wind Speed: ${windValue} km/hr`;
-        // contentBox.style.display = "block";
-        // error.style.display = "none";
-      });
-  });
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + text.value + "&appid=" + apiKey)
+    .then((response) => {
+      if (!response.ok) {
+        contentBox.style.display = "none";
+        alert("Check Your Spelling.");
+        throw new Error("Check Your Spelling.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      let nameValue = data["name"];
+      let humidityValue = data["main"]["humidity"];
+      let tempValue = data["main"]["temp"];
+      let windValue = data["wind"]["speed"];
+      let descValue = data["weather"][0]["description"];
+      let iconValue = data["weather"][0]["icon"];
+
+      city.innerHTML = `${nameValue}`;
+      icon.src = "https://openweathermap.org/img/wn/" + iconValue + ".png";
+      description.innerHTML = `Description: ${descValue}`;
+      temp.innerHTML = `Temperature: ${conversion(tempValue)} °C`;
+      humidity.innerHTML = `Humidity: ${humidityValue} %`;
+      wind.innerHTML = `Wind Speed: ${windValue} km/hr`;
+      contentBox.style.display = "block";
+      error.style.display = "none";
+    });
+});
   
