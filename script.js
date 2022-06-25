73c84c273a09385a2b1078d2cba797a5faab6ef0
koechronix <koechronix@yahoo.com>
2022-06-24 23:29:40 -0400

    create dom elements

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const text = document.querySelector(".text");
 const submitBtn = document.querySelector(".submit");
 
 const contentBox = document.querySelector(".contentBox");
+
+let city = document.querySelector(".city");
+let description = document.querySelector(".description");
+let temp = document.querySelector(".temp");
+let wind = document.querySelector(".wind");
+let humidity = document.querySelector(".humi");
+let icon = document.querySelector(".icon");
